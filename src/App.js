5e7778b8170c08ce15f2c7e450bb52764d6de58b
Jodina753 <82115776+Jodina753@users.noreply.github.com>
2022-05-15 23:54:36 +0100

    second commit - image folders, media queries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Login extends Component {
+  state = {};
+
+  onInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = async () => {
+    await axios.get("http://localhost:8002/add");
+  };
+
+  render() {
+    return (
+      <>
+        <div className="main-container">
+          <div className="header">
+            <h2>Welcome Back</h2>
+          </div>
+
+          <div className="input-container" onInput={this.onInput}>
+            <input type="text" name="email" placeholder="Email"></input>
+
+            <input type="text" name="password" placeholder="Password"></input>
+
+            <button onClick={this.onSubmit}>Sign in</button>
+          </div>
+
+          <div className="footer">
+            <h4>Forgot password?</h4>
+            <span>or</span>
+            <h4>Create an account</h4>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default App;
+export default Login;
