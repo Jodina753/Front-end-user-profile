28ed842f77ec8f313894e2016bad29115c88e889
Jodina753 <82115776+Jodina753@users.noreply.github.com>
2022-05-22 16:19:28 +0100

    width and height adjustments to main container and media queries

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 import axios from "axios";
 
 class Login extends Component {
-  state = {};
+  state = { screen: 1 };
 
   onInput = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  onSubmit = async () => {
-    await axios.get("http://localhost:8002/add");
+  onLogin = async () => {
+    await axios.post("http://localhost:8002/login", this.state); //this is to login to existing account
+  };
+
+  onSignUp = async () => {
+    await axios.post("http://localhost:8002/add", this.state); //this is to create an account
   };
 
   render() {
@@ -21,20 +25,45 @@ class Login extends Component {
             <h2>Welcome Back</h2>
           </div>
 
-          <div className="input-container" onInput={this.onInput}>
+          <div className="input-container">
             <input type="text" name="email" placeholder="Email"></input>
 
             <input type="text" name="password" placeholder="Password"></input>
 
-            <button onClick={this.onSubmit}>Sign in</button>
+            <button onClick={this.onLogin}>Sign in</button>
           </div>
 
           <div className="footer">
             <h4>Forgot password?</h4>
-            <span>or</span>
-            <h4>Create an account</h4>
+            <p>or</p>
+            <button
+              id="sign-up-btn"
+              onClick={() => {
+                this.setState({ screen: 0 });
+              }}
+            >
+              <h4>Create an account</h4>
+            </button>
           </div>
         </div>
+
+        {this.state.screen === 0 && (
+          <div className=" sign-up main-container">
+            <div className="header">
+              <h2>Registration</h2>
+            </div>
+
+            <div className=" sign-up input-container" onInput={this.onInput}>
+              <input type="text" name="email" placeholder="Email"></input>
+
+              <input type="text" name="username" placeholder="Username"></input>
+
+              <input type="text" name="password" placeholder="Password"></input>
+
+              <button onClick={this.onSignUp}>Sign Up!</button>
+            </div>
+          </div>
+        )}
       </>
     );
   }
